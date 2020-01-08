17cb6392eb8c142538988fd2ec773c2f07ab6cb0
YoussefSS <joesamir98@gmail.com>
2020-01-08 16:52:45 +0200

    25. Querying Overlapping Actors

--- Source/ArchitectureExplorer/Private/HandController.cpp
+++ Source/ArchitectureExplorer/Private/HandController.cpp
@@ -20,8 +20,10 @@ AHandController::AHandController()
 void AHandController::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	MotionController->bDisplayDeviceModel = true; // Shows the default controller mesh
+	OnActorBeginOverlap.AddDynamic(this, &AHandController::ActorBeginOverlap);
+	OnActorEndOverlap.AddDynamic(this, &AHandController::ActorEndOverlap);
 }
 
 // Called every frame
@@ -29,5 +31,37 @@ void AHandController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AHandController::ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	bool bNewCanClimb = CanClimb();
+	if (!bCanClimb && bNewCanClimb)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can Climb!"));
+	}
+	bCanClimb = bNewCanClimb;
+
+}
+
+void AHandController::ActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	bCanClimb = CanClimb();
+
+}
+
+bool AHandController::CanClimb() const
+{
+	TArray<AActor*> OverlappingActors;
+	GetOverlappingActors(OverlappingActors);
+	for (AActor* OverlappingActor : OverlappingActors)
+	{
+		if (OverlappingActor->ActorHasTag(TEXT("Climbable")))
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
 
